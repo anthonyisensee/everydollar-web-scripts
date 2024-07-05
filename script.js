ca165277994c9997d6f2f7c373be1b8479ccaf18
anthonyisensee <60198052+anthonyisensee@users.noreply.github.com>
2024-07-05 18:01:04 -0400

    feat: adds exceptions and outputs results to table

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-let total = 0
 let amountArray = []
+let exceptions = ["Operational Expenses", "Roth IRA"]   // Enter funds that should not be included in the table and total
+let total = 0
 
 document.querySelectorAll('.BudgetItemRow:has(.BudgetItemRow-typeIcon)').forEach(row => {
 
     const name = row.querySelector(".BudgetItem-label").dataset.text
     const amount = parseFloat(row.querySelector(".money--remaining").dataset.text.replace(/[$,]/g, ""))
 
-    amountArray.push({ name: name, amount: amount })
+    if (!exceptions.includes(name)) {
+
+        total = total += amount
+        
+        amountArray.push({ name: name, amount: amount })
+
+    }
 
 })
 
-console.log(amountArray)
+amountArray.push({ name: "Total Remaining Funds", amount: total })
+
+console.table(amountArray, ["name", "amount"])
